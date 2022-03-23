43724c5f51388e5f346357fa6b348c5e1a17fab9
jennyrhee <jennyirhee@gmail.com>
2022-03-23 12:33:22 -0500

    Add dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
@@ -18,4 +19,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new Dotenv()
+  ],
 };
