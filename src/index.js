53af9cf11176e0e376ee9313adef360b89a34e11
jennyrhee <jennyirhee@gmail.com>
2022-03-23 15:08:46 -0500

    Show weather data on page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import './css/style.css';
 import { getData } from './api';
 
-(() => {
-  const form = document.getElementById('form');
+function fillDiv(query, text) {
+  const div = document.querySelector(query);
+  div.textContent = text;
+}
+
+function showData(data) {
+  fillDiv('.location', `${data.city}, ${data.state}`)
+  fillDiv('.description', data.main);
+
+  const icon = document.querySelector('.icon');
+  icon.src = `http://openweathermap.org/img/wn/${data.icon}@2x.png`;
+  fillDiv('#main-temp', Math.round(data.temp));
+
+  const etcs = document.querySelectorAll('.etc');
+  etcs[0].textContent = `Feels like: ${Math.round(data.feels_like)}`;
+  etcs[1].textContent = `Humidity: ${data.humidity}%`;
+}
+
+function initializeForm(formId) {
+  const form = document.getElementById(formId);
   form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const data = getData(e.target[0].value);
-    console.log(data);
+    getData(e.target[0].value).then(data => showData(data));
   })
+}
+
+(() => {
+  getData('Lafayette').then(data => {
+    console.log(data);
+    showData(data);
+  });
+  initializeForm('form');
 })();
